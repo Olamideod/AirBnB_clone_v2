383f832e3df324af8d208e99283ab1d56a3b8665
Olamideod <oluwamusiwaolamide@gmail.com>
2024-01-26 05:10:51 -0800

    Write a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
+# Comment out or remove this line:
+# app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
-
+  """Print web"""
+  return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+  app.run(debug=True, host='0.0.0.0', port=5000)
+
